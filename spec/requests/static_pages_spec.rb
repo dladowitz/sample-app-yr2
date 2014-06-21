633b4ed9ab5de2dd7b85755dcb84bc149bdb57a7
David Ladowitz <david@ladowitz.com>
2014-06-21 10:01:26 -0700

    Finished Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "Static Pages" do
       visit static_pages_home_path
       expect(page).to have_content "Sample App"
     end
+
+    it "should have the right title" do
+      visit static_pages_home_path
+      expect(page).to have_title "Ruby on Rails Tutorial Sample App | Home"
+    end
   end
 
   describe "Help Page" do
@@ -13,16 +18,22 @@ describe "Static Pages" do
       visit static_pages_help_path
       expect(page).to have_content "Help"
     end
+
+    it "should have the right title" do
+      visit static_pages_help_path
+      expect(page).to have_title "Ruby on Rails Tutorial Sample App | Help"
+    end
   end
-
-
-
-
 
   describe "About Page" do
     it "should have content 'About Us'" do
       visit static_pages_about_path
       expect(page).to have_content("About Us")
+    end
+
+    it "should have the right title" do
+      visit static_pages_about_path
+      expect(page).to have_title "Ruby on Rails Tutorial Sample App | About"
     end
   end
 end
